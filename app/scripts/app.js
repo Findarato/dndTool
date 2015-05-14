2f59a7924ac173509b91b18454cf5bfea09108c8
Joseph Harry <findarato@gmail.com>
2015-05-14 18:47:04 -0500

    The start of the new character cards are in. These will show each of the characters that you have created. Once I figure out how I am going to show them and make it not look terrible./

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,7 @@
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
   app.appName = 'Dragons';
+  app.version = '0.0.2';
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
